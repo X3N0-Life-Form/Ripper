4be095db65bdbe10f2697a6959ee8934a3f569c0
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-11 22:00:14 +0100

    new test resource: test03

--- test/tests/URLS.java
+++ test/tests/URLS.java
@@ -2,12 +2,19 @@ package tests;
 
 public class URLS {
 
-	public static final String TEST01_ARCHIVE = "test-resources/archives/test01.7z";
-	public static final String TEST02_ARCHIVE = "test-resources/archives/test02.7z";
 	public static final String TEST_RESOURCES_RESULTS = "test-resources/results";
+	
+	public static final String TEST01_ARCHIVE = "test-resources/archives/test01.7z";
 	public static final String RESULTS_TEST01_TXT = TEST_RESOURCES_RESULTS + "/test01/test01.txt";
 	public static final String CONTROL_GROUP_TEST01_TXT = "test-resources/controlGroup/test01.txt";
+	
+	public static final String TEST02_ARCHIVE = "test-resources/archives/test02.7z";
 	public static final String CONTROL_GROUP_TEST02_TXT = "test-resources/controlGroup/test02.txt";
 	public static final String RESULTS_TEST02_TXT = TEST_RESOURCES_RESULTS + "/test02/test02.txt";
-
+	
+	public static final String TEST03_ARCHIVE = "test-resources/archives/test03.zip";
+	public static final String RESULTS_TEST03a_TXT = TEST_RESOURCES_RESULTS + "/test03/test03a.txt";
+	public static final String CONTROL_GROUP_TEST03a_TXT = "test-resources/controlGroup/test03a.txt";
+	public static final String RESULTS_TEST03b_TXT = TEST_RESOURCES_RESULTS + "/test03/test03b.txt";
+	public static final String CONTROL_GROUP_TEST03b_TXT = "test-resources/controlGroup/test03b.txt";
 }
