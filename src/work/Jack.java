fbf43cba46d6b4207ff38fa6eee5b132b3bb7ed2
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-12 16:34:18 +0100

    started work on options file

--- src/work/Jack.java
+++ src/work/Jack.java
@@ -1,5 +1,11 @@
 package work;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -18,14 +24,19 @@ public class Jack {
 	private static String targetDirectory;
 	private static List<String> targets;
 	private static String destinationDirectory;
+	private static String knowledgeDirectory;
 
 	public static void main(String[] args) {
-		dealWithArgs(args);
+		try {
+			dealWithArgs(args);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		
 	}
 
-	protected static void dealWithArgs(String[] args) {
+	protected static void dealWithArgs(String[] args) throws IOException {
 		for (int i = 0; i < args.length; i++) {
 			String arg = args[i];
 			
@@ -44,14 +55,31 @@ public class Jack {
 		}
 	}
 
-	protected static void displayHelp() {
+	public static void displayHelp() {
 		// TODO Auto-generated method stub
 		
 	}
-
-	protected static void readOptions(String fileURL) {
-		// TODO Auto-generated method stub
+	
+	public static void printRecap() {
 		
+	}
+
+	public static void readOptions(String fileURL) throws IOException {
+		File file = new File(fileURL);
+		FileInputStream fis = new FileInputStream(file);
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader reader = new BufferedReader(isr);
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			if (line.startsWith("#target-directory:")) {
+				
+			} else if (line.startsWith("#destination-directory:")) {
+				
+			} else if () {
+				
+			}
+			
+		}
 	}
 
 }
