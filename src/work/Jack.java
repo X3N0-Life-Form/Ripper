24be3622ae659c407743896da3a7ddc1e58b9a8d
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-16 15:49:37 +0100

    fleshed out Jack options system + tests

--- src/work/Jack.java
+++ src/work/Jack.java
@@ -3,9 +3,9 @@ package work;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,25 +15,38 @@ import java.util.List;
  */
 public class Jack {
 	
-	public enum Mode {
+	/**
+	 * Singleton class, do not instantiate.
+	 */
+	private Jack() {}
+	
+	public enum ExtractMode {
 		KEY,
 		KNIFE,
 		EVE
 	}
 	
+	protected enum ReadMode {
+		OPTION,
+		TARGET
+	}
+	private static ReadMode readMode = ReadMode.OPTION;
+	
+	
 	private static String targetDirectory;
-	private static List<String> targets;
+	private static List<String> targets = new ArrayList<String>();
 	private static String destinationDirectory;
 	private static String knowledgeDirectory;
+	
+	private static int numberOfThreads = 1;
 
 	public static void main(String[] args) {
 		try {
 			dealWithArgs(args);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
 
 	protected static void dealWithArgs(String[] args) throws IOException {
@@ -50,6 +63,9 @@ public class Jack {
 			case "--targetDirectory":
 				targetDirectory = args[++i];
 				break;
+			case "--displayKnowledge":
+				// recap knowledge contained in knowledge directory
+				break;
 			}
 			
 		}
@@ -61,7 +77,13 @@ public class Jack {
 	}
 	
 	public static void printRecap() {
-		
+		System.out.println("Target directory: " + targetDirectory);
+		System.out.println("Destination directory: " + destinationDirectory);
+		System.out.println("Knowledge directory: " + knowledgeDirectory);
+		System.out.println("Targets:");
+		for (String target : targets) {
+			System.out.println("\t" + target);
+		}
 	}
 
 	public static void readOptions(String fileURL) throws IOException {
@@ -71,15 +93,60 @@ public class Jack {
 		BufferedReader reader = new BufferedReader(isr);
 		String line = null;
 		while ((line = reader.readLine()) != null) {
-			if (line.startsWith("#target-directory:")) {
-				
+			if (line.startsWith("#")) readMode = ReadMode.OPTION;
+			
+			System.out.println(line);
+			if (readMode == ReadMode.TARGET) {
+				targets.add(line.trim());
+			} else if (line.startsWith("#target-directory:")) {
+				targetDirectory = extractOption(line);
 			} else if (line.startsWith("#destination-directory:")) {
-				
-			} else if () {
-				
+				destinationDirectory = extractOption(line);
+			} else if (line.startsWith("#knowledge-directory:")) {
+				knowledgeDirectory = extractOption(line);
+			} else if (line.startsWith("#targets"))  {
+				readMode = ReadMode.TARGET;
+			} else if (line.startsWith("#threads:")) {
+				numberOfThreads = Integer.parseInt(extractOption(line));
+			} else {
+				System.out.println("Unrecognised option: " + line);
 			}
 			
 		}
+		reader.close();
+	}
+	
+	/**
+	 * 
+	 * @param line
+	 * @return
+	 */
+	protected static String extractOption(String line) {
+		return line.substring(line.indexOf(":") + 1).trim();
+	}
+	
+	///
+	/// Getters/Setters
+	///
+	
+	public static String getTargetDirectory() {
+		return targetDirectory;
+	}
+	
+	public static String getDestinationDirectory() {
+		return destinationDirectory;
+	}
+	
+	public static String getKnowledgeDirectory() {
+		return knowledgeDirectory;
+	}
+	
+	public static int getNumberOfThreads() {
+		return numberOfThreads;
+	}
+	
+	public static List<String> getTargets() {
+		return targets;
 	}
 
 }
---
+++ test/work/JackTests.java
@@ -0,0 +1,39 @@
+package work;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JackTests {
+	
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test_extractOption() {
+		String option = Jack.extractOption("#some option:\ttest   ");
+		assertEquals("test", option);
+	}
+	
+	@Test
+	public void test_readOptions_OK() throws IOException {
+		Jack.readOptions("optionsSample.ini");
+		assertEquals("test-resources/results", Jack.getDestinationDirectory());
+		assertEquals("test-resources/archives", Jack.getTargetDirectory());
+		assertEquals("serializedData", Jack.getKnowledgeDirectory());
+		assertEquals(8, Jack.getNumberOfThreads());
+		assertTrue(Jack.getTargets().contains("test02.7z"));
+		assertTrue(Jack.getTargets().contains("test03.zip"));
+	}
+	
+}
